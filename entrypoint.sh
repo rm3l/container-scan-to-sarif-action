4396a4b661fd06b7117dc59cd49ecf42897bbe04
Armel Soro <armel@rm3l.org>
2021-11-03 09:36:50 +0100

    Change install location of container-scan-to-sarif
    /usr/local/bin is a much more standard location

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,12 @@
 
 CONTAINER_SCAN_TO_SARIF_VERSION="$1"
 echo "Installing container-scan-to-sarif (version $CONTAINER_SCAN_TO_SARIF_VERSION)..."
-mkdir -p ~/var/opt/
 curl -L "https://github.com/rm3l/container-scan-to-sarif/releases/download/${CONTAINER_SCAN_TO_SARIF_VERSION}/container-scan-to-sarif_${CONTAINER_SCAN_TO_SARIF_VERSION}_Linux_x86_64.tar.gz" \
-  | tar zx -C /var/opt/ || exit 1
-chmod +x /var/opt/container-scan-to-sarif
+  | tar zx -C /usr/local/bin/ || exit 1
+chmod +x /usr/local/bin/container-scan-to-sarif
 
 INPUT="$2"
 echo "Now running container-scan-to-sarif against file $INPUT..."
 OUTPUT_FILE="$2"
-/var/opt/container-scan-to-sarif -input "$INPUT" -output "$OUTPUT_FILE" || exit 1
+/usr/local/bin/container-scan-to-sarif -input "$INPUT" -output "$OUTPUT_FILE" || exit 1
 echo "::set-output name=sarif-report-path::$OUTPUT_FILE"
